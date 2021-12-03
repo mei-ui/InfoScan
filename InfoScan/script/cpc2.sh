419d5f0eb8e144e48916220e40088735278b9c7b
mei-ui <64576991+mei-ui@users.noreply.github.com>
2021-12-03 11:30:22 +0800

    Delete InfoScan/script directory

--- InfoScan/script/cpc2.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python CPC2_standalone-1.0.1/bin/CPC2.py -i $1 -o $2
--- InfoScan/script/extract_transcript_name.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-less $1| awk '$3=="transcript"'|grep -Eo 'transcript_id \"\w+\.\w+|transcript_id \"\w+\.\w+\.\w+'|cut -d\" -f 2 |sort|uniq > $2
-

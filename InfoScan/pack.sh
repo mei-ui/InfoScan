70fc18381a800d992b9be634b6029be581bccd8b
mei-ui <64576991+mei-ui@users.noreply.github.com>
2025-04-08 20:45:15 +0800

    Delete InfoScan directory

--- InfoScan/pack.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh  
-exe="InfoScan" #你需要发布的程序名称
-des="/public/home/meisq/05QT/08software/InfoScan" #创建文件夹的位置
-deplist=$(ldd $exe | awk  '{if (match($3,"/")){ printf("%s "),$3 } }')  
-cp $deplist $des
